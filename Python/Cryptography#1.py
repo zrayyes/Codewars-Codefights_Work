39b517e4ea0c9b75f20d577674a0029194916f00
Zuhair Rayyes <rayyes95@hotmail.com>
2017-05-15 23:53:01 +0200

    Create Cryptography#1.py

---
+++ Python/Cryptography#1.py
@@ -0,0 +1,16 @@
+import string
+def caesar_crypto_encode(text, shift):
+    shifter = abs(shift)
+    sign = True if shift > 0 else False
+    abc = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+    crypted = ''
+    if not text or text.isspace() : return ''
+    while shifter > len(abc):
+        
+    for letter in text:
+        if letter in abc:
+            index = abc.index(letter)
+            crypted += abc[index+shifter]
+        else:
+            crypted += letter
+    return crypted
