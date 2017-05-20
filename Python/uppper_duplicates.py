b10e5beeb3ce123a7d36c55aa5e7321ea5f49827
Zuhair Rayyes <rayyes95@hotmail.com>
2017-05-20 23:38:46 +0200

    Update uppper_duplicates.py

--- Python/uppper_duplicates.py
+++ Python/uppper_duplicates.py
@@ -1,19 +1,3 @@
-def reverse(name):
-    new = list(name)
-    temp = []
-    for i in range(len(new)-1):
-        if(new[i] == new[i+1]):
-            temp.append(i)
-            temp.append(i+1)
-    temp = list(set(temp))            
-    for i in temp:
-        if(new[int(i)].isupper()):
-            new[int(i)] = new[int(i)].lower()
-        else:
-            new[int(i)] = new[int(i)].upper()
-    return(''.join(new))
-
-
-# import re
-# def reverse(s):
-#     return re.sub(r'(.)\1+', lambda m: m.group().swapcase(), s)
+import re
+def reverse(s):
+    return re.sub(r'(.)\1+', lambda m: m.group().swapcase(), s)
