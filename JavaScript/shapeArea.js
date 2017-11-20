3ea2e07a09f4ed6a87719bb53c4c0aed2bcfa460
Zuhair Rayyes <zuz1995@yahoo.com>
2017-11-20 21:39:08 +0100

    Fixed shapeArea

--- JavaScript/shapeArea.js
+++ JavaScript/shapeArea.js
@@ -1,13 +1,3 @@
 function shapeArea(n) {
-    let last = 1;
-    let counter = 1;
-    if (n === 1){
-        return last;
-    } else {
-        while (counter < n) {
-            last = last + (counter * 4);
-            counter += 1;
-        }
-    }
-    return last;
+    return n*n + (n-1)*(n-1);
 }
